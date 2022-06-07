06d07860378fe07197edd644f7ec8c27d90592e8
ChrisMak <cmakride@gmail.com>
2022-06-07 13:10:37 -0400

    Edit Cocktail Styling

--- src/pages/EditCocktail/EditCocktail.jsx
+++ src/pages/EditCocktail/EditCocktail.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useLocation } from "react-router-dom"
+import { BsTrash } from 'react-icons/bs'
 
 function EditCocktail({handleUpdateCocktail}) {
     const location = useLocation()
@@ -62,12 +63,12 @@ function EditCocktail({handleUpdateCocktail}) {
     }
 
     return (
-        <>
-        <h1>Edit Cocktail</h1>
-        <form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
+        <div className="max-w-2xl mx-auto bg-white px-16 py-10 rounded-md">
+        <h1 className="py-3 text-center text-3xl font-bold">Edit Cocktail</h1>
+        <form className="grid gap-6 mb-6 lg:grid-cols-1" autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
             <div>
-            <label className="mt-8 text text-2xl font-bold" htmlFor="name-input">Cocktail Name:</label>
-                <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            <label className="mt-8 text text-l font-medium" htmlFor="name-input">Cocktail Name:</label>
+                <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     name="name"
                     onChange={handleChange}
@@ -76,7 +77,7 @@ function EditCocktail({handleUpdateCocktail}) {
 
                 <br />
                 <label className="mt-8 text text-2xl font-bold" htmlFor="method-input">Method:</label>
-                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="method"
+                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="method"
                     onChange={handleChange}
                     value={formData.method}>
                     <option></option>
@@ -86,7 +87,7 @@ function EditCocktail({handleUpdateCocktail}) {
                 </select> <br />
                 <label className="mt-8 text text-2xl font-bold" htmlFor="garnish-input">Garnish:</label>
                 <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     name="garnish"
                     onChange={handleChange}
@@ -94,7 +95,7 @@ function EditCocktail({handleUpdateCocktail}) {
                     required
                 /> <br />
                 <label  className="mt-8 text text-2xl font-bold" htmlFor="servedin-input">Served in:</label>
-                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     name="served_in"
                     onChange={handleChange}
                     value={formData.served_in}
@@ -112,13 +113,13 @@ function EditCocktail({handleUpdateCocktail}) {
             </div>
            
             <div>
-            <div>
-                <label htmlFor="photo-upload">
+            <div className="grid gap-6 mb-6 lg:grid-cols-1">
+                <label className="mt-1 text text-2xl font-bold" htmlFor="photo-upload">
                     Upload Photo:
                 </label>
                 <input
                     type="file"
-                    className="form-control"
+                    className="mt-1 text text-xl font-bold bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none focus:border-transparent dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 w-full box-border"
                     id="photo-upload"
                     name="image"
                     onChange={handleChangePhoto}
@@ -126,8 +127,8 @@ function EditCocktail({handleUpdateCocktail}) {
             </div>
             </div>
             <div>
-                <button type="submit" disabled={!validForm}>
-                    Save
+                <button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" type="submit" disabled={!validForm}>
+                    Save Cocktail
                 </button>
             </div>
         </form>
@@ -139,13 +140,13 @@ function EditCocktail({handleUpdateCocktail}) {
                 <h4
                     className="mt-8 text text-2xl font-bold">Please Add an Ingredient</h4>
                 <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     name="ingredient"
                     value={inputValue}
                     onChange={handleIngredientChange}
                 /> <br />
-                <button className="text-white bg-yellow-500 p-2 rounded-md" type='submit'
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type='submit'
 
                 >
                     Add Ingredient
@@ -154,15 +155,15 @@ function EditCocktail({handleUpdateCocktail}) {
             <h4 className="mt-8 text text-2xl font-bold">Ingredients</h4>
             <ul>
                 {arrayIngredients.map((ingredient, idx) => (
-                    <li key={idx}>
+                    <li className="flex items-center gap-3" key={idx}>
                         <h2>{ingredient}</h2>
-                        <button className="text-white bg-red-500 p-2 rounded-md"type="button" onClick={() => handleRemoveIngredient(idx)}>
-                            Remove
+                        <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 mr-1 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" type="button" onClick={() => handleRemoveIngredient(idx)}>
+                        <BsTrash className="text-xl" />
                         </button>
                     </li>
                 ))}
             </ul>
-        </>
+        </div>
     )
 }
 
